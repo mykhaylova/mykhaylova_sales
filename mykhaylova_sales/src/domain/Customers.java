490c1bb3859f74379fdb75ebf464f49e27b5642a
mykhaylova <mariya_lvovna@yahoo.com>
2016-05-18 01:40:24 +0200

    Class Customers(ArrayList)

---
+++ mykhaylova_sales/src/domain/Customers.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Customers {
+
+	private ArrayList<Customer> customers;
+
+	public Customers() {
+		customers = new ArrayList<>();
+	}	
+}
