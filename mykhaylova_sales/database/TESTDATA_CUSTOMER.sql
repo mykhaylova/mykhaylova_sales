096239a72e48fa616bb7a73a9cbd0f436cb6dbbc
mykhaylova <mariya_lvovna@yahoo.com>
2016-05-22 15:44:45 +0200

    Customer testdata

---
+++ mykhaylova_sales/database/TESTDATA_CUSTOMER.sql
@@ -0,0 +1,6 @@
+insert into customer values (1006, 'Annalena', 5358);
+insert into customer values (1007,	'Liv',	1378);
+insert into customer values (1008,	'Marie',	-24);
+insert into customer values (1009,	'Hira',	1798);
+insert into customer values (1010,	'Celina',	3281);
+insert into customer values (1011,	'Kim',	2574);
